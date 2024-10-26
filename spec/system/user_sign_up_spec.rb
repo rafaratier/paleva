bf227d4652380402959504f139201849b97f169d
rafaratier <rafaelratier@hotmail.com>
2024-10-26 17:06:29 -0300

    tests: refatoção para incluir validação de cpf

--- spec/system/user_sign_up_spec.rb
+++ spec/system/user_sign_up_spec.rb
@@ -9,15 +9,17 @@ describe 'User sign up' do
       within('form') do
         fill_in 'Nome', with: 'Jeff'
         fill_in 'Sobrenome', with: ''
-        fill_in 'CPF', with: '00000000000'
+        fill_in 'CPF', with: CPF.generate(true)
         fill_in 'E-mail', with: 'jeffbezos@gmail.com'
         fill_in 'Senha', with: '2*6JEFFbb001'
         fill_in 'Confirme sua senha', with: '2*6JEFFbb001'
         click_on 'Criar conta'
       end
 
+      expect(page).to have_content 'Não foi possível salvar usuário: 2 erro'
       expect(page).to have_content 'Sobrenome não pode ficar em branco'
-      expect(current_path).to eq new_user_session_path
+      expect(page).to have_content 'Sobrenome é muito curto'
+      expect(User.count).to eq 0
     end
   end
 end
