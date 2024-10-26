3d690a678e15dfdce50a2ed543b41d3a6f45eb82
rafaratier <rafaelratier@hotmail.com>
2024-10-26 19:02:37 -0300

    feat: adiciona validação de unicidade para atributos email e cpf

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,10 @@ class User < ApplicationRecord
   has_one :establishment, foreign_key: :owner_id, dependent: :destroy
 
   validates :name, :lastname, :personal_national_id, presence: true
-  validates :name, length: { minimum: 2, maximum: 25 }
-  validates :lastname, length: { minimum: 2, maximum: 25 }
+  validates :name, :lastname, length: { minimum: 2, maximum: 25 }
+  validates :email, uniqueness: true
   validates :password, length: { minimum: 12 }
+  validates :personal_national_id, uniqueness: true
   validate :personal_national_id_must_be_valid_CPF
 
   private
