27ff37be6361695b21f64256b72943eac6517133
rafaratier <rafaelratier@hotmail.com>
2024-10-26 17:07:14 -0300

    feat: adiciona cadastro de usuários

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :lastname, :personal_national_id ])
+  end
 end
