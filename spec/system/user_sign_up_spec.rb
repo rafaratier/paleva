c823e56398e5868214a63dd99832010440c55292
rafaratier <rafaelratier@hotmail.com>
2024-10-26 19:01:52 -0300

    tests: testa valicações de unicidade de email e cpf durante criação de conta

--- spec/system/user_sign_up_spec.rb
+++ spec/system/user_sign_up_spec.rb
@@ -42,6 +42,60 @@ describe 'User sign up' do
       expect(page).to have_content 'E-mail não pode ficar em branco'
       expect(User.count).to eq 0
     end
+
+    it 'can not create account when email is already in use' do
+      same_email = 'jeffbezos@amazon.com'
+
+      User.create!(
+        name: 'Jeff',
+        lastname: 'Bezos',
+        personal_national_id: valid_cpf,
+        email: same_email,
+        password: 'jeff2*6bezos'
+      )
+
+      visit new_user_session_path
+      click_on 'Criar conta'
+
+      within('form') do
+        fill_in 'Nome', with: 'Bill'
+        fill_in 'Sobrenome', with: 'Gates'
+        fill_in 'CPF', with: CPF.generate(true)
+        fill_in 'E-mail', with: same_email
+        fill_in 'Senha', with: 'bill2*6gates'
+        fill_in 'Confirme sua senha', with: 'bill2*6gates'
+        click_on 'Criar conta'
+      end
+
+      expect(page).to have_content 'Não foi possível salvar usuário'
+      expect(page).to have_content 'E-mail já está em uso'
+    end
+
+    it 'can not create account when personal national ID is already in use' do
+      User.create!(
+        name: 'Jeff',
+        lastname: 'Bezos',
+        personal_national_id: valid_cpf,
+        email: 'jeffbezos@amazon.com',
+        password: 'jeff2*6bezos'
+      )
+
+      visit new_user_session_path
+      click_on 'Criar conta'
+
+      within('form') do
+        fill_in 'Nome', with: 'Bill'
+        fill_in 'Sobrenome', with: 'Gates'
+        fill_in 'CPF', with: valid_cpf
+        fill_in 'E-mail', with: 'billgates@outlook.com'
+        fill_in 'Senha', with: 'bill2*6gates'
+        fill_in 'Confirme sua senha', with: 'bill2*6gates'
+        click_on 'Criar conta'
+      end
+
+      expect(page).to have_content 'Não foi possível salvar usuário'
+      expect(page).to have_content 'CPF já está em uso'
+    end
   end
 
   context 'with valid data' do
