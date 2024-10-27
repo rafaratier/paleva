00f7f9d11c1155d800035b22d6b6a30502e8231d
rafaratier <rafaelratier@hotmail.com>
2024-10-27 11:36:29 -0300

    feat: redirecionamento para cadastro obrigatório de estabelecimento

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,21 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :require_registration
+
+  before_action :set_user_full_name
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :lastname, :personal_national_id ])
+  end
+
+  def require_registration
+    if user_signed_in? && current_user.establishment.nil? && request.path != new_establishment_path && request.path != destroy_user_session_path
+      redirect_to new_establishment_path
+    end
+  end
+
+  def set_user_full_name
+    @user_full_name = "#{current_user.name} #{current_user.lastname}" if current_user
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
   def index
-    @user_full_name = "#{current_user.name} #{current_user.lastname}"
   end
 end
