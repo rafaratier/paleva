8e5d28546d65d14d1362bcf47b10c80a8d996ed9
rafaratier <rafaelratier@hotmail.com>
2024-10-27 23:22:12 -0300

    fix: remove validação email duplicada no model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,6 @@ class User < ApplicationRecord
   validates :email, :personal_national_id, uniqueness: true
   validate :personal_national_id_must_be_valid_CPF
 
-  validates :email, email: true
-
   private
 
   def personal_national_id_must_be_valid_CPF
