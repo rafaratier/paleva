3c1c4fa1e2eb54a05e38499bbc259b04b694d976
rafaratier <rafaelratier@hotmail.com>
2024-10-27 21:26:06 -0300

    feat: validação de atributos de address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :establishment
+
+  validates :street_name, :street_number, :neighborhood, :city, :state, :country, presence: true
+
+  validates :street_name, :neighborhood, :city, :state, :country, length: { minimum: 4, maximum: 50 }
 end
