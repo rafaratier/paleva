092fed6ba172ac7e1200bde07674cde1b2aafbd8
rafaratier <rafaelratier@hotmail.com>
2024-11-01 00:05:22 -0300

    tests: impede redirecionamento até que complete registro de horario de funcionamento

--- spec/system/business_hours/user_registers_business_hours_spec.rb
+++ spec/system/business_hours/user_registers_business_hours_spec.rb
@@ -25,5 +25,57 @@ describe "Business Hours registration" do
       expect(establishment.business_hours.present?).to be false
       expect(current_path).to eq new_establishment_business_hour_path(establishment.id)
     end
+
+    it "can't visit other page until registers business hours" do
+      user = User.create!(
+      name: 'Jeff',
+      lastname: 'Bezos',
+      personal_national_id: CPF.generate(true),
+      email: 'jeffbezos@amazon.com',
+      password: 'jeff2*6bezos')
+
+      establishment = Establishment.create!(
+      trade_name: 'Papega',
+      legal_name: 'Pede e Pega ME',
+      business_national_id: CNPJ.generate(true),
+      phone: '0123456789',
+      email: 'contato@papega.com.br',
+      owner: user)
+
+      login_as(user)
+      visit root_path
+
+      expect(current_path).to eq new_establishment_business_hour_path(establishment.id)
+
+      visit establishment_path
+
+      expect(establishment.business_hours.present?).to be false
+      expect(current_path).to eq new_establishment_business_hour_path(establishment.id)
+    end
+
+    it "can logout without having registered business hours" do
+      user = User.create!(
+      name: 'Jeff',
+      lastname: 'Bezos',
+      personal_national_id: CPF.generate(true),
+      email: 'jeffbezos@amazon.com',
+      password: 'jeff2*6bezos')
+
+      establishment = Establishment.create!(
+      trade_name: 'Papega',
+      legal_name: 'Pede e Pega ME',
+      business_national_id: CNPJ.generate(true),
+      phone: '0123456789',
+      email: 'contato@papega.com.br',
+      owner: user)
+
+      login_as(user)
+      visit root_path
+
+      click_on 'Sair'
+
+      expect(establishment.business_hours.present?).to be false
+      expect(current_path).to eq new_user_session_path
+    end
   end
 end
