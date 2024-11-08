fa7d47548f328b8850822e7373eafbce365f68cd
rafaratier <rafaelratier@hotmail.com>
2024-11-07 23:40:34 -0300

    tests: refatora testes para sign out de usuários

--- spec/system/authentication/users_sign_out/user_sign_out_spec.rb
+++ spec/system/authentication/users_sign_out/user_sign_out_spec.rb
@@ -1,23 +1,131 @@
 require 'rails_helper'
 
 describe "User sign out" do
-  it "should redirect to sign in page" do
-    user = User.create!(
-    name: 'Jeff',
-    lastname: 'Bezos',
-    personal_national_id: CPF.generate(true),
-    email: 'jeffbezos@amazon.com',
-    password: 'jeff2*6bezos')
+  context "when user is establishment Owner" do
+    it "establishment owner can sign out without registering an establishment" do
+      owner = User.create!(
+        role: 'owner',
+        name: 'Jeff',
+        lastname: 'Bezos',
+        personal_national_id: CPF.generate(true),
+        email: 'jeffbezos@amazon.com',
+        password: 'jeff2*6bezos')
 
-    login_as(user)
-    visit root_path
+      login_as(owner)
+      visit root_path
 
-    within('header') do
-      click_on 'Sair'
+      within('header') do
+        click_on 'Sair'
+      end
+
+      expect(owner.owned_establishment.present?).to be false
+      expect(page).to have_content 'Entrar'
+      expect(current_path).to eq new_user_session_path
     end
 
-    expect(current_path).to eq new_user_session_path
-    expect(page).to have_content 'Entrar'
-    expect(page).not_to have_content 'Jeff Bezos'
+    it "establishment owner can sign out without registering business hours" do
+      owner = User.create!(
+        role: 'owner',
+        name: 'Jeff',
+        lastname: 'Bezos',
+        personal_national_id: CPF.generate(true),
+        email: 'jeffbezos@amazon.com',
+        password: 'jeff2*6bezos')
+
+      owner.create_owned_establishment!(
+        trade_name: 'Papega',
+        legal_name: 'Pede e Pega ME',
+        business_national_id: CNPJ.generate(true),
+        phone: '0123456789',
+        email: 'contato@papega.com.br')
+
+      login_as(owner)
+      visit root_path
+
+      within('header') do
+        click_on 'Sair'
+      end
+
+      expect(owner.owned_establishment.present?).to be true
+      expect(owner.owned_establishment.opened_for_business?).to be false
+      expect(page).to have_content 'Entrar'
+      expect(current_path).to eq new_user_session_path
+    end
+
+    it "establishment owner can sign out from the homepage after registering establishment and business hours" do
+      owner = User.create!(
+        role: 'owner',
+        name: 'Jeff',
+        lastname: 'Bezos',
+        personal_national_id: CPF.generate(true),
+        email: 'jeffbezos@amazon.com',
+        password: 'jeff2*6bezos')
+
+      owner.create_owned_establishment!(
+        trade_name: 'Papega',
+        legal_name: 'Pede e Pega ME',
+        business_national_id: CNPJ.generate(true),
+        phone: '0123456789',
+        email: 'contato@papega.com.br')
+
+      owner.owned_establishment.business_hours.create!(
+        day_of_week: 'sunday',
+        status: 'opened',
+        open_time: '10:00',
+        close_time: '20:00')
+
+      login_as(owner)
+      visit root_path
+
+      within('header') do
+        click_on 'Sair'
+      end
+
+      expect(owner.owned_establishment.present?).to be true
+      expect(owner.owned_establishment.opened_for_business?).to be true
+      expect(page).to have_content 'Entrar'
+      expect(current_path).to eq new_user_session_path
+    end
+  end
+
+  context "when user is establishment Employee" do
+    it "establishment employee can sign out from the homepage" do
+      owner = User.create!(
+        role: 'owner',
+        name: 'Jeff',
+        lastname: 'Bezos',
+        personal_national_id: CPF.generate(true),
+        email: 'jeffbezos@amazon.com',
+        password: 'jeff2*6bezos')
+
+      owner.create_owned_establishment!(
+        trade_name: 'Papega',
+        legal_name: 'Pede e Pega ME',
+        business_national_id: CNPJ.generate(true),
+        phone: '0123456789',
+        email: 'contato@papega.com.br')
+
+      owner.owned_establishment.pending_employees.create!(
+        email: 'billgates@outlook.com',
+        personal_national_id: '556.452.500-01')
+
+      employee = User.create!(
+        role: 'employee',
+        name: 'Bill',
+        lastname: 'Gates',
+        personal_national_id: '556.452.500-01',
+        email: 'billgates@outlook.com',
+        password: 'bill2*6gates')
+
+      login_as(employee)
+      visit root_path
+
+      within('header') do
+        click_on 'Sair'
+      end
+
+      expect(page).to have_content 'Entrar'
+      expect(current_path).to eq new_user_session_path
+    end
   end
 end
